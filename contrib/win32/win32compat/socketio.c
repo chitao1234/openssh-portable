7c44d9c3176aaa70cb5b931371a9dcd0ecc4c47b
Xuntao Chi <chotaotao1qaz2wsx@gmail.com>
2026-06-01 15:55:21 +0800

    win32: simplify hostname lookup

--- contrib/win32/win32compat/socketio.c
+++ contrib/win32/win32compat/socketio.c
@@ -32,6 +32,7 @@
 #include <ws2tcpip.h>
 #include <mswsock.h>
 #include <errno.h>
+#include <limits.h>
 #include <stddef.h>
 #include "w32fd.h"
 #include "utf.h"
@@ -984,29 +985,13 @@ socketio_on_select(struct w32_io* pio, BOOL rd)
 int
 w32_gethostname(char *name_utf8, size_t len)
 {
-	char* tmp_name_utf8 = NULL;
-
-	if (pIsWindows8OrGreater()) {
-		/* TODO - GetHostNameW not present in Win7, do GetProcAddr on Win8+*/
-	        /*
-		wchar_t name_utf16[256];
-		if (GetHostNameW(name_utf16, 256) == SOCKET_ERROR) {
-	                errno = errno_from_WSALastError();
-	                return -1;
-	        }
-
-	        if ((tmp_name_utf8 = utf16_to_utf8(name_utf16)) == NULL ||
-	                strlen(tmp_name_utf8) >= len) {
-	                errno = EFAULT; //??
-	                return -1;
-	        }
-
-	        memcpy(name_utf8, tmp_name_utf8, strlen(tmp_name_utf8) + 1);
-	        free(tmp_name_utf8);
-	        return 0; */
-		return gethostname(name_utf8, (int)len);
-	} else
-		return gethostname(name_utf8, (int)len);
+	if (len > INT_MAX)
+		len = INT_MAX;
+	if (gethostname(name_utf8, (int)len) == SOCKET_ERROR) {
+		errno = errno_from_WSALastError();
+		return -1;
+	}
+	return 0;
 }
 
 void
